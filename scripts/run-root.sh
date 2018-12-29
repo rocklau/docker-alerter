cb92065f8f73eff00b3e9dc8e78cfe51abd24a0c
Rocky Liu <rocklau83@gmail.com>
2018-12-30 01:09:08 +0800

    curl post docker events

--- scripts/run-root.sh
+++ scripts/run-root.sh
@@ -3,56 +3,45 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-echo "[i] create /etc/ssmtp/ssmtp.conf"
-{
-    echo "mailhub=${SSMTP_SERVER}";
-    echo "AuthUser=${SSMTP_USER}";
-    echo "AuthPass=${SSMTP_PASS}";
-    echo "FromLineOverride=yes";
-    echo "UseTLS=yes";
-} > /etc/ssmtp/ssmtp.conf
-
-unset SSMTP_SERVER SSMTP_USER SSMTP_PASS
-
 while true; do
-  curl --silent --show-error --unix-socket "${DOCKER_SOCKET}" -H "Accept: application/json" "http:/v1.24/events?filters=%7B%22type%22%3A%5B%22container%22%5D%7D" |
-    while read line; do
-        status=`echo "${line}" | jq .status -r`
-        if [[ "${status:0:5}" == "exec_" ]]; then
-            continue
-        fi
-        if [[ "${status}" == "attach" ]]; then
-            continue
-        fi
+	docker events --filter 'type=container' --filter 'event=stop' --filter 'event=start' --filter 'event=die'  --filter 'event=destory' --format '{{json .}}' |
+		while read line; do
+			status=$(echo "${line}" | jq .status -r)
+			if [[ "${status:0:5}" == "exec_" ]]; then
+				continue
+			fi
+			if [[ "${status}" == "attach" ]]; then
+				continue
+			fi
 
-        name=`echo "${line}" | jq .Actor.Attributes.name -r`
-        image=`echo "${line}" | jq .Actor.Attributes.image -r`
-        exitCode=`echo "${line}" | jq .Actor.Attributes.exitCode -r`
-        json=`echo "${line}" | jq '.'`
+			name=$(echo "${line}" | jq .Actor.Attributes.name -r)
+			image=$(echo "${line}" | jq .Actor.Attributes.image -r)
+			exitCode=$(echo "${line}" | jq .Actor.Attributes.exitCode -r)
+			json=$(echo "${line}" | jq '.')
 
-        if [[ "${status}" == "die" ]]; then
-            logs="$(curl --silent --show-error --unix-socket /var/run/docker.sock "http:/v1.24/containers/${name}/logs?stdout=1&stderr=1&timestamps=1&since=$(($(date +%s)-60))")"
-        else
-            logs=""
-        fi
+			if [[ "${status}" == "die" ]]; then
+				logs="$(curl --silent --show-error --unix-socket /var/run/docker.sock "http:/v1.24/containers/${name}/logs?stdout=1&stderr=1&timestamps=1&since=$(($(date +%s) - 60))")"
+			else
+				logs=""
+			fi
 
-        if [[ "${exitCode}" == "null" ]]; then
-            exitCode=""
-        else
-            exitCode="exitcode ${exitCode}"
-        fi
-            ssmtp "${MAIL_TO}" << EOF
-To: ${MAIL_TO}
-From: ${MAIL_FROM}
-Subject: ${MAIL_PREFIX}${name}
+			if [[ "${exitCode}" == "null" ]]; then
+				exitCode=""
+			else
+				exitCode="exitcode ${exitCode}"
+			fi
 
-Event: ${status} ${exitCode}
-Container: ${name}
-Image: ${image}
-
-${json}
-
-${logs}
+			curl -X POST \
+				http://127.0.0.1/hook \
+				-H 'Content-Type: application/json' \
+				-d @- <<EOF
+{
+    "title": "${name} ${image}",  
+    "description":"Event: ${status} ${exitCode}",  
+    "from":"rocky",  
+    "touser":"liuyan",
+    "url":"https://docker.com" 
+}
 EOF
-    done
+		done
 done
